ead3f5e24440a1d1a1d1007807a5f7c20b41fa75
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-04 14:19:14 +0500

    add appendFile()

---
+++ data/data.js
@@ -0,0 +1,1 @@
+{"name":"bolachalar"}{"name":"eshmat"}
--- server.js
+++ server.js
@@ -1,5 +1,35 @@
-// ! js synchrone language
-// ! node.js asynchrone language
-const os = require("os");
-console.log(os.version());
-console.log(os.totalmem() / 1024);
+// ! javascript is synchronic language
+// ! node.js is asynchronic language
+// const os = require("os");
+// console.log(os.version());
+// console.log(os.totalmem() / 1024);
+
+const http = require("http");
+const fs = require("fs");
+
+// ! fs metodlari!
+/*
+fs.appendFile("./data/data.js") ----> oldindan yaratilgan data folderiga data.js file ni yaratib ichiga content joylab beradi. har yangi ma`lumot jo`natganimizada data.js da mavjud contentni o`chirmay davomidan qo`shib qo`yadi!
+*/
+
+const server = http.createServer((req, res) => {
+  if (req.method == "POST") {
+    if (req.url.substring(1) == "newUser") {
+      req.on("data", (data) => {
+        let user = JSON.parse(data);
+
+        // fs.readFile("./data/data.js", (err, data) => {
+        //   const arr = jSON.parse(data);
+        //   arr.push(user);
+        // });
+        fs.appendFile("./data/data.js", JSON.stringify(user), (err) => {
+          if (err) throw err; //note: "Throw" runtime paytda xatolik bo`sa chiqarib beradi
+          console.log("created");
+        });
+      });
+      res.end("data keldi");
+    }
+  }
+});
+
+server.listen(9000, console.log(9000));
